aa699dd7df3f6e32c4a76d003c137cf4e4e150e9
Abiram <abirambijoy@gmail.com>
2025-11-27 20:49:51 +0530

    Updated October Newsletter

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -4,6 +4,7 @@ import 'react-photo-view/dist/react-photo-view.css';
 import { translations } from '../translations';
 
 const newsletters = [
+  { src: "/NL/NLOct2025.jpg", alt: "October 2025 Newsletter" },
   { src: "/NL/NLSept2025.jpg", alt: "September 2025 Newsletter" },
   { src: "/NL/NLAugust2025.png", alt: "August 2025 Newsletter" },
   { src: "/NL/NLJuly2025.png", alt: "July 2025 Newsletter" },
