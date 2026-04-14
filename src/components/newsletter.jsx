bb7890b3cacf49a73591495a2704e51a938ca3ce
Abiram <abirambijoy@gmail.com>
2026-04-14 13:28:27 +0530

    April Calender added

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -4,6 +4,7 @@ import 'react-photo-view/dist/react-photo-view.css';
 import { translations } from '../translations';
 
 const newsletters = [
+  { src: "/NL/2026/3.png", alt: "March 2026 Newsletter", date: "Mar 2026" },
   { src: "/NL/2026/1.png", alt: "January 2026 Newsletter", date: "Jan 2026" },
   { src: "/NL/2025/12.png", alt: "December 2025 Newsletter", date: "Dec 2025" },
   { src: "/NL/2025/11.jpg", alt: "November 2025 Newsletter", date: "Nov 2025" },
